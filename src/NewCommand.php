e58480fb157f0140d8c73be9a8e923cf5f5e40b6
marveloo <36376767+marveloo@users.noreply.github.com>
2020-07-27 19:24:43 +0300

    Revert changes made in v1.3.7
    We do not want to set any explicit permissions on writable directories,
    so that directories can inherit permissions by following Unix
    permissions model.

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -9,8 +9,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 use ZipArchive;
 
@@ -61,7 +59,6 @@ class NewCommand extends Command
 
         $this->download($zipFile = $this->makeFilename(), $this->getVersion($input))
              ->extract($zipFile, $directory)
-             ->prepareWritableDirectories($directory, $output)
              ->cleanUp($zipFile);
 
         $composer = $this->findComposer();
@@ -192,27 +189,6 @@ class NewCommand extends Command
         @chmod($zipFile, 0777);
 
         @unlink($zipFile);
-
-        return $this;
-    }
-
-    /**
-     * Make sure the storage and bootstrap cache directories are writable.
-     *
-     * @param  string  $appDirectory
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return $this
-     */
-    protected function prepareWritableDirectories($appDirectory, OutputInterface $output)
-    {
-        $filesystem = new Filesystem;
-
-        try {
-            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR.'bootstrap/cache', 0755, 0000, true);
-            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR.'storage', 0755, 0000, true);
-        } catch (IOExceptionInterface $e) {
-            $output->writeln('<comment>You should verify that the "storage" and "bootstrap/cache" directories are writable.</comment>');
-        }
 
         return $this;
     }
